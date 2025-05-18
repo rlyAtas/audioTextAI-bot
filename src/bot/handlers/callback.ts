e0e01da005224de14d5bbd1165da7d89de5aefcd
Stanislav Shevchenko <stas.s.shevchenko@gmail.com>
2025-05-18 14:03:41 +0200

    minor fix

--- src/bot/handlers/callback.ts
+++ src/bot/handlers/callback.ts
@@ -1,9 +1,6 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { download } from './callback/download.js';
 import { startLanguageSet } from './callback/startLanguageSet.js';
-import { getLogger } from '../../classes/Logger.js';
-
-const logger = getLogger();
 
 const routes = [
   { test: (data: string) => data.split('_')[0] === 'download', handler: download },
@@ -11,18 +8,9 @@ const routes = [
 ];
 
 export function handlerCallback(bot: TelegramBot, query: TelegramBot.CallbackQuery) {
-  try {
-    logger.debug(`[handlerCallback] query = ${JSON.stringify(query)}`);
-    for (const route of routes) {
-      if (query.data && route.test(query.data)) {
-        route.handler(bot, query);
-      }
+  for (const route of routes) {
+    if (query.data && route.test(query.data)) {
+      route.handler(bot, query);
     }
-  } catch (err: unknown) {
-    logger.error(`[handlerCallback] query = ${JSON.stringify(query)}, err = ${err}`);
-    bot.answerCallbackQuery(query.id, {
-      text: '⚠️ Произошла техническая ошибка. Мы уже работаем над её устранением.',
-      show_alert: true,
-    });
   }
 }
