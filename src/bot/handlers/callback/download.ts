24239ab2e11b0e63b1f624e12b52701f90dfaf86
Stanislav Shevchenko <stas.s.shevchenko@gmail.com>
2025-05-18 11:42:58 +0200

    minor fix

--- src/bot/handlers/callback/download.ts
+++ src/bot/handlers/callback/download.ts
@@ -7,7 +7,7 @@ const logger = getLogger();
 
 export async function download(bot: TelegramBot, query: TelegramBot.CallbackQuery) {
   try {
-    logger.debug(`[download] query = ${JSON.stringify(query)}`);
+    logger.debug(`[bot/handlers/callback/download] query = ${JSON.stringify(query)}`);
     const chatId = query.message!.chat.id;
     const fileName = query.data?.split('_')[1];
     const filePath = path.join(CWD, `/texts/${chatId}/${fileName}.txt`);
@@ -16,7 +16,7 @@ export async function download(bot: TelegramBot, query: TelegramBot.CallbackQuer
       caption: '📥 Файл с результатом транскрибации',
     });
   } catch (err: unknown) {
-    logger.error(`[download] query = ${JSON.stringify(query)}, err = ${err}`);
+    logger.error(`[bot/handlers/callback/download] query = ${JSON.stringify(query)}, err = ${err}`);
     bot.answerCallbackQuery(query.id, {
       text: '⚠️ Произошла техническая ошибка. Мы уже работаем над её устранением.',
       show_alert: true,
