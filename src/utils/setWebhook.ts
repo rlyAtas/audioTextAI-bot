3394d1f2d8b5f6060f88686846a0c29977081cc8
Stanislav Shevchenko <stas.s.shevchenko@gmail.com>
2025-05-26 20:36:19 +0200

    log webhook

--- src/utils/setWebhook.ts
+++ src/utils/setWebhook.ts
@@ -1,14 +1,20 @@
 import dotenv from 'dotenv';
 import TelegramBot from 'node-telegram-bot-api';
+import { getLogger } from '../classes/Logger.js';
+
+const logger = getLogger();
 
 dotenv.config();
 
 export async function setWebhook(webhook: string, token: string): Promise<void> {
   try {
+    logger.debug(`[utils/setWebhook] webhook = ${webhook}, token = ${token.split(':')[0]}...`);
     const bot = new TelegramBot(token);
     const isSetting = await bot.setWebHook(`${webhook}/bot${process.env.BOT_TOKEN}`);
     if (!isSetting) throw new Error('Webhook not set');
   } catch (error: unknown) {
-    console.log(error);
+    logger.error(
+      `[utils/setWebhook] webhook = ${webhook}, token = ${token.split(':')[0]}..., error = ${error}`,
+    );
   }
 }
